bb45e83db761ac2c812c58cc60f0955d94d1cd37
jonathanmir <jonathanmir8@gmail.com>
2023-02-12 21:27:01 -0300

    finalRoutesAndDiagram

--- database.sql
+++ database.sql
@@ -2,33 +2,37 @@ CREATE TYPE OS AS ENUM ('windows', 'linux', 'macos');
 
 CREATE TABLE IF NOT EXISTS developer_infos(
  "id" SERIAL PRIMARY KEY,
- "preferedOS" OS NOT NULL,
- "developerSince" DATE NOT NULL
+  "developerSince" DATE NOT NULL,
+ "preferedOS" OS NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS developers (
 "id" SERIAL PRIMARY KEY,
 "name" VARCHAR(50) NOT NULL,
-"email" VARCHAR(50) NOT NULL UNIQUE
+"email" VARCHAR(50) NOT NULL UNIQUE,
+"developerInfoId" INTEGER UNIQUE,
+FOREIGN KEY ("developerInfoId") REFERENCES developer_infos("id") ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS projects (
 "id" SERIAL PRIMARY KEY,
-"name" VARCHAR(50) NOT NULL,
-"description" VARCHAR NOT NULL,
-"estimatedTime" VARCHAR(20) NOT NULL,
-"repository" VARCHAR(120) NOT NULL,
-"startDate" DATE NOT NULL,
-"endDate" DATE
+"projectName" VARCHAR(50) NOT NULL,
+"projectDescription" VARCHAR NOT NULL,
+"projectEstimatedTime" VARCHAR(20) NOT NULL,
+"projectRepository" VARCHAR(120) NOT NULL,
+"projectStartDate" DATE NOT NULL,
+"projectEndDate" DATE,
+"developerId" INTEGER,
+FOREIGN KEY ("developerId") REFERENCES developers("id") ON DELETE CASCADE
 ); 
 
 
 CREATE TABLE IF NOT EXISTS technologies (
 "id" SERIAL PRIMARY KEY,
-"name" VARCHAR(30) NOT NULL
+"technologyName" VARCHAR(30) NOT NULL
 );
 
-INSERT INTO technologies ("name")
+INSERT INTO technologies ("technologyName")
 VALUES 
 ('JavaScript'),
 ('Python'),
@@ -42,6 +46,9 @@ VALUES
 
 CREATE TABLE IF NOT EXISTS projects_technologies (
 "id" SERIAL PRIMARY KEY,
-"addedIn" DATE NOT NULL
+"addedIn" DATE NOT NULL DEFAULT NOW(),
+"technologyId" INTEGER NOT NULL,
+"projectId" INTEGER NOT NULL,
+FOREIGN KEY ("technologyId") REFERENCES technologies("id"),
+FOREIGN KEY ("projectId") REFERENCES projects("id")
 );
-
